1450dc585d127bba021076f810f9380a0d0eef3c
Romin Adi Santoso <romin991@gmail.com>
2017-05-10 19:11:12 +0700

    add user info model

---
+++ MPMFinance/MPMUserInfo.h
@@ -0,0 +1,15 @@
+//
+//  MPMUserInfo.h
+//  MPMFinance
+//
+//  Created by Romin Adi Santoso on 5/10/17.
+//  Copyright © 2017 MPMFinance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPMUserInfo : NSObject
++(NSDictionary*)getUserInfo;
++(void)deleteUserInfo;
++(void)save:(NSDictionary*)dictionary;
+@end
