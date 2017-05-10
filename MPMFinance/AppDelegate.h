187f2afc60150c7b5416c32f7512ec7251da6bb3
Rudy Suharyadi <rudy.suharyadi@gmail.com>
2017-05-10 16:33:43 +0700

    Initialize project

---
+++ MPMFinance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MPMFinance
+//
+//  Created by Rudy Suharyadi on 5/10/17.
+//  Copyright © 2017 MPMFinance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MPMFinance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MPMFinance
+//
+//  Created by Rudy Suharyadi on 5/10/17.
+//  Copyright © 2017 MPMFinance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
