0f5995828db75b83e02de1f2a5b1e9f2eb29c18c
Rudy Suharyadi <rudy.suharyadi@gmail.com>
2017-05-11 12:33:50 +0700

    MenuViewController

---
+++ MPMFinance/ViewController/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  MPMFinance
+//
+//  Created by Rudy Suharyadi on 5/11/17.
+//  Copyright © 2017 MPMFinance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
